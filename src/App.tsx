04ea648ce72719dc389a544532789738b8b0d6d9
Hanyu Duan <HanyuDuan@outlook.com>
2024-04-24 07:04:26 +0800

    use /api/get_user_loggedin/ to check if logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
+import { useState, useEffect } from 'react';
 import LoginSignupPage from './LoginSignupPage.tsx'
 import UserHomePage from './UserHomePage.tsx'
 
-function get_login_status() {
-  return false;
+import axios from 'axios';
+
+function checkUserLogin(setOnPage: Function) {
+  axios.get('/api/get_user_loggedin/')
+  .then(resp => {
+    console.log('got resp:', resp)
+    if (resp.data.loggedin) {
+      console.log('is loggedin')
+      setOnPage('home')
+    } else {
+      console.log('not loggedin yet')
+      setOnPage('login')
+    }
+  }).catch(error => {
+    console.error('error:', error)
+    alert('unknown error')
+  });
+  return "good or bad, let's see console";
 }
 
+
 function App() {
-  let isLoggedIn = get_login_status();
-  let page = <LoginSignupPage />;
-  if (isLoggedIn) {
-    page = <UserHomePage />;
-  }
+  const [onPage, setOnPage] = useState('home');
+
+  useEffect(() => {
+    checkUserLogin(setOnPage);
+  }, []);
+
   return (
     <>
-      {page}
+      {onPage === "login" && <LoginSignupPage />}
+      {onPage === "home" && <UserHomePage />}
     </>
   );
 }
