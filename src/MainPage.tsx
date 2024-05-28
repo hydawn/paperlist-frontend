1cc2059af98ce20b5770525cc622697235e25953
Hanyu Duan <HanyuDuan@outlook.com>
2024-05-28 21:31:32 +0800

    restruct main page

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import SideBar from './SideBar.tsx';
-import UserMainPage from './UserMainPage.tsx';
+import UserHomePage from './UserHomePage.tsx'
+import PaperPage from './PaperPage.tsx'
+import PaperSetPage from './PaperSetPage.tsx';
 import { Container, Row, Col } from 'react-bootstrap';
 import './MainPage.css';
 
@@ -11,18 +13,27 @@ interface Prop {
 export default function MainPage({setLoginStatus}: Prop) {
   const [onPage, setOnPage] = useState('home');
 
-  const sideBarNames = ['主页', '论文', '论文库'];
-  function UserPage() {
-    return <Container fluid>
-      <Row>
-        <Col xs={2} id="sidebar-wrapper">
-          <SideBar onPage={onPage} setOnPage={setOnPage} sideBarNames={sideBarNames} />
-        </Col>
-        <Col xs={10} id="main-content-wrapper">
-          <UserMainPage onPage={onPage} setOnPage={setOnPage} setLoginStatus={setLoginStatus} />
-        </Col>
-      </Row>
-    </Container>;
+  function MainPageWindow() {
+    switch (onPage) {
+      case '论文库':
+        return <PaperSetPage setOnPage={setOnPage} />;
+      case '论文':
+        return <PaperPage setOnPage={setOnPage} />;
+      default:
+        return <UserHomePage setLoginStatus={setLoginStatus} />;
+    }
   }
-  return <UserPage />;
+
+  const sideBarNames = ['主页', '论文', '论文库'];
+
+  return <Container fluid>
+    <Row>
+      <Col xs={2} id="sidebar-wrapper">
+        <SideBar onPage={onPage} setOnPage={setOnPage} sideBarNames={sideBarNames} />
+      </Col>
+      <Col xs={10} id="main-content-wrapper">
+        <MainPageWindow />
+      </Col>
+    </Row>
+  </Container>;
 }
